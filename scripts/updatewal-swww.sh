bbb4b053186da66261dc9808312cb54cdc3bb2c2
DevArtist254 <kevinwasonga415@outlook.com>
2024-04-18 03:18:49 +0000

    hell yeah

---
+++ scripts/updatewal-swww.sh
@@ -0,0 +1,6 @@
+
+
+source = "~/.cache/wal/colors.sh"
+
+swaybg -i ~/.config/wallpaper
+
---
+++ scripts/wallpaper-swww.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+IMAGE_FOLDER="/home/devartist/.config/wallpaper"
+
+if [ ! -d "$IMAGE_FOLDER" ]; then
+    echo "Error: image folder not found."
+    exit 1
+fi
+
+image_files=("$IMAGE_FOLDER"/*)
+
+if [ ${#image_files[@]} -eq 0 ]; then 
+    echo "Error: no image file found."
+    exit 1
+fi
+
+random_index=$((RANDOM % ${#image_files[@]}))
+
+random_image=${image_files[$random_index]}
+
+wal -q -i $random_image
+
+exec swaybg -i $random_image
+
+echo "Wallpaper set to: $random_image" 
